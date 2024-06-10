918df603cfa220c57c8d034757471a5d69bf6067
jackhclee <38123458+jackhclee@users.noreply.github.com>
2024-06-11 00:58:33 +0100

    FE2O3-0002: init

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,59 @@
 use domain::*;
 
+fn print_book(discounted: &impl Discounted) -> () {
+    println!("This is print_book {}", discounted.less(0.9))
+}
+
 fn main() {
     println!("Hello, world!");
-    let book = Book {
+    let mut book = Book {
         title: String::from("AAA"),
-        price: 100
+        price: 100,
     };
-    println!("{:?} discounted_price ${}", book, book.discounted_price(0.9));
+
+    print_book(&book);
+    println!(
+        "{:?} discounted_price ${} (less ${})",
+        book,
+        book.discounted_price(0.9),
+        book.less(0.9)
+    );
+    book.update_price(0.9);
+    println!(
+        "{:#?} discounted_price ${} (less ${})",
+        book,
+        book.discounted_price(0.9),
+        book.less(0.9)
+    );
 }
 
 pub mod domain {
 
+    pub trait Discounted {
+        fn discounted_price(&self, discount_rate: f32) -> f32;
+
+        fn less(&self, discount_rate: f32) -> f32;
+
+        fn update_price(&mut self, discount_rate: f32) -> ();
+    }
+
     #[derive(Debug)]
     pub struct Book {
         pub title: String,
-        pub price: i32
+        pub price: i32,
     }
 
-    impl Book {
-        pub fn discounted_price(&self, discount_rate: f32) -> f32 {
+    impl Discounted for Book {
+        fn discounted_price(&self, discount_rate: f32) -> f32 {
             self.price as f32 * discount_rate
+        }
+
+        fn less(&self, discount_rate: f32) -> f32 {
+            self.price as f32 - self.discounted_price(discount_rate)
+        }
+
+        fn update_price(&mut self, discount_rate: f32) -> () {
+            self.price = self.discounted_price(discount_rate) as i32;
         }
     }
 }
